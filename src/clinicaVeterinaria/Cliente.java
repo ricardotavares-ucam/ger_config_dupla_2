1e7a3c5d428f09867991d9d48acbe66aa051a7e8
naugo <naugopgomes@gmail.com>
2021-10-22 17:23:39 -0300

    criação da classe Especie e modificação na classe Cliente

--- src/clinicaVeterinaria/Cliente.java
+++ src/clinicaVeterinaria/Cliente.java
@@ -5,9 +5,57 @@ public class Cliente
 	private String nom_cli;
 	private String end_cli;
 	private String tel_cli;
-	private String cep_cli;
+	private Long cep_cli;
 	private String email_cli;
 	
+
+	public String getNom_cli()
+	{
+		return nom_cli;
+	}
+
+	public void setNom_cli(String nom_cli) 
+	{
+		this.nom_cli = nom_cli;
+	}
+
+	public String getEnd_cli() 
+	{
+		return end_cli;
+	}
+
+	public void setEnd_cli(String end_cli) 
+	{
+		this.end_cli = end_cli;
+	}
+
+	public String getTel_cli() {
+		return tel_cli;
+	}
+
+	public void setTel_cli(String tel_cli) 
+	{
+		this.tel_cli = tel_cli;
+	}
+
+	public Long getCep_cli() {
+		return cep_cli;
+	}
+
+	public void setCep_cli(Long cep_cli) 
+	{
+		this.cep_cli = cep_cli;
+	}
+
+	public String getEmail_cli() 
+	{
+		return email_cli;
+	}
+
+	public void setEmail_cli(String email_cli) 
+	{
+		this.email_cli = email_cli;
+	}
 	
 	public int reg_Cli()
 	{
@@ -22,46 +70,6 @@ public class Cliente
 		
 	}
 	
-
-	public String getNom_cli() {
-		return nom_cli;
-	}
-
-	public void setNom_cli(String nom_cli) {
-		this.nom_cli = nom_cli;
-	}
-
-	public String getEnd_cli() {
-		return end_cli;
-	}
-
-	public void setEnd_cli(String end_cli) {
-		this.end_cli = end_cli;
-	}
-
-	public String getTel_cli() {
-		return tel_cli;
-	}
-
-	public void setTel_cli(String tel_cli) {
-		this.tel_cli = tel_cli;
-	}
-
-	public String getCep_cli() {
-		return cep_cli;
-	}
-
-	public void setCep_cli(String cep_cli) {
-		this.cep_cli = cep_cli;
-	}
-
-	public String getEmail_cli() {
-		return email_cli;
-	}
-
-	public void setEmail_cli(String email_cli) {
-		this.email_cli = email_cli;
-	}
 	
 	
 }
---
+++ src/clinicaVeterinaria/Especie.java
@@ -0,0 +1,23 @@
+package clinicaVeterinaria;
+
+public class Especie 
+{
+	
+	private String nom_esp;
+	
+
+	public String getNom_esp() 
+	{
+		return nom_esp;
+	}
+
+	
+	public void setNom_esp(String nom_esp) 
+	{
+		this.nom_esp = nom_esp;
+	}
+	
+	
+	
+
+}
