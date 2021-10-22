a791c7edb957a26907c47bb71af1f0ccdf1b3b12
naugo <naugopgomes@gmail.com>
2021-10-22 14:21:13 -0300

    criação da classe cliente

---
+++ src/clinicaVeterinaria/Cliente.java
@@ -0,0 +1,67 @@
+package clinicaVeterinaria;
+
+public class Cliente 
+{
+	private String nom_cli;
+	private String end_cli;
+	private String tel_cli;
+	private String cep_cli;
+	private String email_cli;
+	
+	
+	public int reg_Cli()
+	{
+		return 0;
+		
+	}
+	
+	
+	public String con_Cli()
+	{
+		return "";
+		
+	}
+	
+
+	public String getNom_cli() {
+		return nom_cli;
+	}
+
+	public void setNom_cli(String nom_cli) {
+		this.nom_cli = nom_cli;
+	}
+
+	public String getEnd_cli() {
+		return end_cli;
+	}
+
+	public void setEnd_cli(String end_cli) {
+		this.end_cli = end_cli;
+	}
+
+	public String getTel_cli() {
+		return tel_cli;
+	}
+
+	public void setTel_cli(String tel_cli) {
+		this.tel_cli = tel_cli;
+	}
+
+	public String getCep_cli() {
+		return cep_cli;
+	}
+
+	public void setCep_cli(String cep_cli) {
+		this.cep_cli = cep_cli;
+	}
+
+	public String getEmail_cli() {
+		return email_cli;
+	}
+
+	public void setEmail_cli(String email_cli) {
+		this.email_cli = email_cli;
+	}
+	
+	
+}
